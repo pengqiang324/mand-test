25489090fc0a5579cf45958a2c49fe8c7b103c93
pengqiang <839556387@qq.com>
2019-12-07 11:38:01 +0800

    修改global.less Home.vue文件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="home">
+    <div class="md-example-child-swiper-0">
+      <md-swiper
+      ref="swiper"
+      :is-prevent="false"
+      :useNative-driver="false"
+    >
+      <md-swiper-item :key="$index" v-for="(item, $index) in simple">
+        <div
+          class="banner-item"
+          :style="{'background': `${item.color}`}">{{item.text}}</div>
+      </md-swiper-item>
+    </md-swiper>
+    </div>
     <md-action-bar :actions="data"/>
   </div>
 </template>
 
 <script>
-import {ActionBar, Toast} from 'mand-mobile'
+import {ActionBar, Toast, Swiper, SwiperItem} from 'mand-mobile'
+import simple from 'mand-mobile/components/swiper/demo/data/simple'
 export default {
   name: "home",
 
   components: {
+    [Swiper.name]: Swiper,
+    [SwiperItem.name]: SwiperItem,
     [ActionBar.name]: ActionBar,
   },
 
@@ -21,6 +37,7 @@ export default {
           onClick: this.handleClick,
         },
       ],
+      simple,
     }
   },
 
@@ -29,10 +46,31 @@ export default {
       Toast.succeed('Click')
     },
   },
+
+  created() {
+    console.log(simple)
+  }
 };
 </script>
 <style lang="less">
 .home {
   font-size: 16Px;
+  .md-example-child-swiper-0 {
+    height: 250px;
+    .banner-item {
+      float: left;
+      width: 100%;
+      height: 100%;
+      line-height: 250px;
+      text-align: center;
+      font-size: 28px;
+      color: #FFF;
+      box-align: center;
+      align-items: center;
+      box-pack: center;
+      justify-content: center;
+      text-decoration-line: none;
+    }
+  }
 }
 </style>
